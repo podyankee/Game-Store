599b10185a1aa58cff8f70d16e36e3de2890d18c
podyankee <podyankee@gmail.com>
2026-06-01 09:50:04 +0300

    block-hero added

--- plugins/blocks-gamestore/build/block-hero/index.asset.php
+++ plugins/blocks-gamestore/build/block-hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '07eecb7d5882d0960558');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '49273039c231256a1aa7');
--- plugins/blocks-gamestore/build/block-hero/view.asset.php
+++ plugins/blocks-gamestore/build/block-hero/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '081ab4e44935882b9cfc');
+<?php return array('dependencies' => array(), 'version' => 'ee2f48988aad04457ff7');
